d5fd0f6aacda9c44939bed37b8f08988df5c08e8
sagarhutagi <sagarmh6364@gmail.com>
2026-07-26 12:41:32 +0530

    match statement

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,8 +1,17 @@
-struct User {
-    hello: String,
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
 
-fn main() {
-    println!("Hello, world!");
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
 
+fn main() {}
